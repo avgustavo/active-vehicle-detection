af7aa35a92439d48408e952aba6365a6a646a936
Gustavo Vieira de Andrade <avgustavo@alu.ufc.br>
2025-07-25 09:48:22 -0300

    add treino_transitar

---
+++ tt.py
@@ -0,0 +1,81 @@
+import argparse
+import time
+from ultralytics import YOLO
+
+from pipeline import calculate_time
+#################################### COnstantes #####################################
+YAML_PATH = 'treino_transitar/tt.yaml'
+
+def printf(message: str):
+    print('=' * 100)
+    print(message)
+    print('=' * 100)
+
+def main(name: str):
+
+    printf('Avaliação do modelo yolo em treino_transitar')
+
+    model = YOLO('yolo11n.pt')
+
+    results_0 = model.val(
+        data=YAML_PATH,
+        split='val',
+        name=f'{name}_11n_test',
+        project='runstt',
+        classes=[0, 1, 2, 3, 5, 6, 7, 9],
+    )
+
+    res_csv = results_0.to_csv()
+    with open(f'runstt/{name}_11n_test/results.csv', 'w') as f:
+        f.write(res_csv)
+
+    printf('Treinamento completo em treino_transitar')
+    model = YOLO('yolo11n.pt')
+    t1 = time.time()
+    model.train(
+        data=YAML_PATH,
+        epochs=25,
+        imgsz=640,
+        batch=16,
+        device=[0, 1],
+        project='runstt',
+        name=name,
+        plots=True,
+        optimizer='AdamW',
+        lr0=0.0001,
+        momentum=0.9,
+        freeze=10
+    )
+    t2 = time.time()
+    print(f'Tempo total de treinamento: {calculate_time(t1, t2)} segundos')
+
+    best_model_p = f'runsbdd/{name}/weights/best.pt'
+    model = YOLO(best_model_p)
+
+    m_val = model.val(
+        data=YAML_PATH,
+        split='val',
+        name=f'{name}_val',
+        project='runstt',
+    )
+    print(f"  > mAP50-95 (val): {m_val.box.map:.4f}")
+    print(f"  > mAP50 (val):    {m_val.box.map50:.4f}")
+    print(f"  > mAP (val):      {m_val.box.map75:.4f}")
+
+    results = model(source='treino_transitar/images/val', batch=16, project='runstt', name=f'{name}_pred', stream=True)
+
+
+    i = 0
+    for result in results:
+        result.save()
+        i+=1
+        if i > 10:
+            break
+    t3 = time.time()
+
+if __name__ == '__main__':
+
+    parse = argparse.ArgumentParser(description="Treinamento do modelo YOLO com o dataset BDD10K")
+    parse.add_argument('--name', type=str, required=True, help="Nome do experimento")
+    args = parse.parse_args()
+    main(args.name)
